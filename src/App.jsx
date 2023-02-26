030abc3702d4faf8551e606b6f79fe4dd7d74d3d
jurgenstevens <jurgen.stevens@yahoo.com>
2023-02-26 02:15:05 -0600

    Set up the structure of the navbar with the business title and list items.

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,6 @@ import Navbar from './components/Navbar.jsx'
 function App() {
   return (
     <div className="App">
-      <h1>Hello from App.jsx!</h1>
       <Navbar />
     </div>
   )
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,9 +1,18 @@
 import React from 'react'
 
 export default function Navbar() {
+    // <img src="../../public/GBP-Logo.jpeg" />
   return (
-    <div className='text-white'>
-        <h1>Navbar</h1>
+    <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
+        <h1 className='text-3xl font-bold text-[#5D9CF3]'>Garage Builders Plus</h1>
+        <ul className='flex'>
+            <li className='p-4'>Home</li>
+            <li className='p-4'>About Us</li>
+            <li className='p-4'>Services</li>
+            <li className='p-4'>Gallery</li>
+            <li className='p-4'>Quote</li>
+            <li className='p-4'>Contact</li>
+        </ul>
     </div>
   )
 }
