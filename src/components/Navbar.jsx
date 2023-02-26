e84869a4480ef370c09243df3c849eb7b34839de
jurgenstevens <jurgen.stevens@yahoo.com>
2023-02-26 16:46:30 -0600

    Stub up hamburger dropdown menu w/o functionality.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,14 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AiOutlineClose, AiOutlineMenu } from 'react-icons/ai'
 
 export default function Navbar() {
+const [nav, setNav] = useState(false)
+
     // <img src="../../public/GBP-Logo.jpeg" />
   return (
     <div className='flex justify-between items-center h-24 max-w-[1240px] mx-auto px-4 text-white'>
         <h1 className='text-3xl font-bold text-[#5D9CF3]'>Garage Builders Plus</h1>
-        <ul className='flex'>
+        <ul className='flex hidden'>
             <li className='p-4'>Home</li>
             <li className='p-4'>About Us</li>
             <li className='p-4'>Services</li>
@@ -13,6 +16,19 @@ export default function Navbar() {
             <li className='p-4'>Quote</li>
             <li className='p-4'>Contact</li>
         </ul>
+        <div>
+          <AiOutlineMenu size={20}/>
+        </div>
+        <div className='fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] m-4'>
+          <ul className='uppercase p-4' >
+            <li className='p-4 border-b border-gray-600 '>Home</li>
+            <li className='p-4 border-b border-gray-600 '>About Us</li>
+            <li className='p-4 border-b border-gray-600 '>Services</li>
+            <li className='p-4 border-b border-gray-600 '>Gallery</li>
+            <li className='p-4 border-b border-gray-600 '>Quote</li>
+            <li className='p-4'>Contact</li>
+          </ul>
+        </div>
     </div>
   )
 }
