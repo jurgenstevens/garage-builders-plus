217c80712db2aa71e777e5ee35a87305255eecf3
jurgenstevens <jurgen.stevens@yahoo.com>
2023-03-05 01:29:03 -0600

    Fix hamburger menu condition.

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -30,9 +30,9 @@ export default function Navbar() {
             <li className='p-4'><Link to="/contact">Contact</Link></li>
         </ul>
         <div onClick={handleNav} className='block md:hidden'>
-          {!nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
+          {nav ? <AiOutlineClose size={20} /> : <AiOutlineMenu size={20} />}
         </div>
-        <div className={!nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] m-4 ease-in-out duration-500' : 'fixed left-[-100%]'}>
+        <div className={nav ? 'fixed left-0 top-0 w-[60%] h-full border-r border-r-gray-900 bg-[#000300] m-4 ease-in-out duration-500' : 'fixed left-[-100%]'}>
           <ul className='uppercase p-4' >
             <li className='p-4 border-b border-gray-600 '><Link>Home</Link></li>
             <li className='p-4 border-b border-gray-600 '><Link>About Us</Link></li>
