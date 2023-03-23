ecfa694f96761668d1955f138083b89666d9a7db
jurgenstevens <jurgen.stevens@yahoo.com>
2023-03-22 23:45:26 -0500

    Update layout for services page.

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 import ServicesImgs from '../components/Services/ServicesImgs'
-import Services from '../components/Services/Services'
+import Service from '../components/Services/Services'
 
 export default function Services() {
   return (
-    <div className='flex'>
-      <Services />
+    <div className='flex qfont-bold gap-4 mt-4 justify-center'>
+      <Service />
       <ServicesImgs />
     </div>
   )
