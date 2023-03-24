ed9ac9421871120b9b95f2905dc46d7ecc52b991
jurgenstevens <jurgen.stevens@yahoo.com>
2023-03-23 23:58:39 -0500

    Center components for about page.

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -4,11 +4,13 @@ import Reviews from '../components/About/Reviews.jsx'
 
 export default function About() {
   return (
-    <div className='qfont-bold flex gap-4 mt-4 justify-center'>
-      {/* <div className="text-[#5D9CF2] qfont-bold flex gap-4 mt-4 justify-center"></div> */}
-        <AboutUs />
-        <Reviews />
-        
+    <div className='flex flex-wrap mx-auto max-w-screen-lg'>
+      <div className='qfont-bold flex gap-4 mt-4 justify-center'>
+        {/* <div className="text-[#5D9CF2] qfont-bold flex gap-4 mt-4 justify-center"></div> */}
+          <AboutUs />
+          <Reviews />
+          
+      </div>
     </div>
   )
 }
