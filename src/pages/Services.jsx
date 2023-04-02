f041db457a28140b1f44933841d11be223c10994
jurgenstevens <jurgen.stevens@yahoo.com>
2023-04-02 03:15:22 -0500

    Update services styling by centering the service and removing the images to use in gallery.

--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -2,44 +2,53 @@ import React from 'react'
 // import ServicesImgs from '../components/Services/ServicesImgs'
 // import Service from '../components/Services/Services'
 
-export default function Services() {
-  return (
-    <div className='flex flex-wrap mx-auto max-w-screen-lg p-8'>
-      <div className='w-full p-4'>
-        <div className='h-full rounded-lg overflow-hidden shadow-md bg-black text-white'>
-          <h1 className='text-2xl font-bold p-4'>Concrete</h1>
-          <p className='p-4 text-[#5D9CF3]'>
-            New garage concrete pad, sidewalk and driveways.
-          </p> 
+  export default function Services() {
+    return (
+      <div className='container flex flex-wrap mx-auto max-w-screen-lg p-2 max-h-screen'>
+        <div className='w-full p-4'>
+          <div className='h-full rounded-lg overflow-hidden shadow-md bg-black text-white flex items-center max-h-150'>
+            <div className='flex-1'>
+              <h1 className='text-3xl font-bold p-4 text-center'>Concrete</h1>
+              <p className='p-4 text-[#5D9CF3]'>
+              Our concrete service provides high-quality installation of new garage concrete pads, sidewalks, and driveways for residential and commercial properties.
+              </p> 
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className='w-full p-4'>
-        <div className='h-full rounded-lg overflow-hidden shadow-md bg-black text-white'>
-          <h1 className='text-2xl font-bold p-4'>Siding</h1>
-          <p className='p-4 text-[#5D9CF3]'>
-            We can replace home and garage siding.
-          </p>
+  
+        <div className='w-full p-4'>
+          <div className='h-full rounded-lg overflow-hidden shadow-md bg-black text-white flex items-center max-h-150'>
+            <div className="flex-1">
+              <h1 className='text-3xl font-bold p-4 text-center'>Siding</h1>
+              <p className='p-4 text-[#5D9CF3]'>
+              The garage siding service provided offers professional installation of durable and energy-efficient siding materials to enhance the look and value of your garage.
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className='w-full p-4'>
-        <div className='h-full rounded-lg overflow-hidden shadow-md bg-black text-white'>
-          <h1 className='text-2xl font-bold p-4'>Garage Door and Motor Replacement</h1>
-          <p className='p-4 text-[#5D9CF3]'>
-            Replace an existing garage door and motor with a new one to improve security, energy efficiency and convenience.
-          </p>
+  
+        <div className='w-full p-4'>
+          <div className='h-full rounded-lg overflow-hidden shadow-md bg-black text-white flex items-center max-h-150'>
+            <div className="flex-1">
+              <h1 className='text-3xl font-bold p-4 text-center'>Garage Door and Motor Replacement</h1>
+              <p className='p-4 text-[#5D9CF3]'>
+                Upgrade your garage with a new door and motor for improved security, energy efficiency, and convenience.
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className='w-full p-4'>
-        <div className='h-full rounded-lg overflow-hidden shadow-md bg-black text-white'>
-          <h1 className='text-2xl font-bold p-4'>Garage Door Repair</h1>
-          <p className='p-4 text-[#5D9CF3]'>
-            We repair residential and commercial doors and motors.
-          </p>
+  
+        <div className='w-full p-4'>
+          <div className='h-full rounded-lg overflow-hidden shadow-md bg-black text-white flex items-center max-h-150'>
+            <div className="flex-1">
+              <h1 className='text-3xl font-bold p-4 text-center'>Garage Door Repair</h1>
+              <p className='p-4 text-[#5D9CF3]'>
+                We repair residential and commercial garage doors and motors for improved security, energy efficiency and convenience.
+              </p>
+            </div>
+          </div>
         </div>
-      </div>
     </div>
-  );
+
+  )
 }
