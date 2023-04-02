d88b510714ab8d45927301c59bc4140c3e12c32c
jurgenstevens <jurgen.stevens@yahoo.com>
2023-04-02 00:25:31 -0500

    Temporarily remove About page child components and use div for easier styling.

--- src/components/About/Reviews.jsx
+++ src/components/About/Reviews.jsx
@@ -5,8 +5,8 @@ export default function Reviews() {
     <div className='bg-[#F0ECE1] text-bod rounded-2xl p-8 gap-4 mt-4'>
         <h1 className='font-bold text-center'>Reviews Component!</h1>
         <ul>
-          <li>- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus earum ducimus, dolores sapiente molestiae voluptas iusto aspernatur in est ratione aliquam esse maxime, nihil quasi architecto culpa optio quo praesentium.</li>
-          <li>- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus earum ducimus, dolores sapiente molestiae voluptas iusto aspernatur in est ratione aliquam esse maxime, nihil quasi architecto culpa optio quo praesentium.</li>
+          <li> I got my garage door replaced last summer. I high recommend! Fast, efficient and so kind. - Jessica Washington</li>
+          <li>- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus earum ducimus, dolores sapiente molestiae voluptas iusto aspernatur in est ratione aliquam esse maxime, nihil quasi architecto culpa optio quo praesentium. - Jane Doe</li>
         </ul>
     </div>
   )
