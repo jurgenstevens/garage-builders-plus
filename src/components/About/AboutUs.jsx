802212e8b822cd181f5ba74a635d769d239677b0
jurgenstevens <jurgen.stevens@yahoo.com>
2023-04-02 00:51:31 -0500

    Update header font size and color as well as including another lorem impsum review.

--- src/components/About/AboutUs.jsx
+++ src/components/About/AboutUs.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function AboutUs() {
   return (
     <div className='max-w-lg w-full mx-auto md:mx-0 md:mr-4 md:mb-0 mb-4 bg-black text-[#5D9CF3] flex-grow rounded-2xl p-8 gap-4 mt-4'>
-      <h1 className='font-bold text-center text-3xl'>About Us!</h1>
+      <h1 className='font-bold text-center text-3xl text-white mb-4'>About Us!</h1>
       <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate consectetur molestiae similique id asperiores, tempore amet possimus suscipit facilis soluta et! Veniam maiores consequuntur modi quibusdam non nostrum aliquam tenetur.
       Cum nobis saepe culpa sapiente quas dolor doloremque illo neque qui tempore explicabo doloribus delectus voluptas nisi sint, soluta veritatis, eum id ut commodi ipsum reprehenderit, totam placeat optio. Libero.
       Tempore, voluptatem. Quod laudantium doloribus non rem porro ipsam! Consectetur illum corporis blanditiis quod, maiores doloremque suscipit optio! Temporibus esse cum repellat aliquam nam dolorem assumenda odit minima quos possimus?
--- src/components/About/Reviews.jsx
+++ src/components/About/Reviews.jsx
@@ -3,10 +3,15 @@ import React from 'react'
 export default function Reviews() {
   return (
     <div className='max-w-lg w-full mx-auto md:mx-0 md:ml-4 bg-black text-[#5D9CF3] flex-grow rounded-2xl p-8 gap-4 mt-4'>
-    <h1 className='font-bold text-center text-3xl'>Reviews Component!</h1>
+    <h1 className='font-bold text-center text-3xl text-white mb-4'>Reviews</h1>
     <ul>
-      <li> I got my garage door replaced last summer. I high recommend! Fast, efficient and so kind. - Jessica Washington</li>
-      <li>- Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus earum ducimus, dolores sapiente molestiae voluptas iusto aspernatur in est ratione aliquam esse maxime, nihil quasi architecto culpa optio quo praesentium. - Jane Doe</li>
+      <li> 
+        <p>I got my garage door replaced last summer. I high recommend! Fast, efficient and so kind. - <b>Jessica Washington</b></p>
+      </li>
+      <br />
+      <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus earum ducimus, dolores sapiente molestiae voluptas iusto aspernatur in est ratione aliquam esse maxime, nihil quasi architecto culpa optio quo praesentium. - <b>Jane Doe</b></li>
+      <br />
+      <li>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Doloribus earum ducimus, dolores sapiente molestiae voluptas iusto aspernatur in est ratione aliquam esse maxime, nihil quasi architecto culpa optio quo praesentium. - <b>John Jones</b></li>
     </ul>
   </div>
   )
