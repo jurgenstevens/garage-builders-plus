195054340aafc175f679db2f93804a0a9d8c566b
jurgenstevens <jurgen.stevens@yahoo.com>
2023-04-17 23:52:43 -0500

    Update About Us component in About page component.

--- src/components/About/AboutUs.jsx
+++ src/components/About/AboutUs.jsx
@@ -4,10 +4,13 @@ export default function AboutUs() {
   return (
     <div className='max-w-lg w-full mx-auto md:mx-0 md:mr-4 md:mb-0 mb-4 bg-black text-[#5D9CF3] flex-grow rounded-2xl p-8 gap-4 mt-4'>
       <h1 className='font-bold text-center text-3xl text-white mb-4'>About Us!</h1>
-      <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptate consectetur molestiae similique id asperiores, tempore amet possimus suscipit facilis soluta et! Veniam maiores consequuntur modi quibusdam non nostrum aliquam tenetur.
-      Cum nobis saepe culpa sapiente quas dolor doloremque illo neque qui tempore explicabo doloribus delectus voluptas nisi sint, soluta veritatis, eum id ut commodi ipsum reprehenderit, totam placeat optio. Libero.
-      Tempore, voluptatem. Quod laudantium doloribus non rem porro ipsam! Consectetur illum corporis blanditiis quod, maiores doloremque suscipit optio! Temporibus esse cum repellat aliquam nam dolorem assumenda odit minima quos possimus?
-      Repudiandae accusamus magni iure modi reiciendis nostrum, tempora voluptate rem eaque amet, saepe repellat maxime. Aspernatur voluptates saepe dolorem nisi reiciendis? Quae neque sapiente tempora, qui ullam minus dolorum a!</p>
+      <p>Garage Builders Plus Inc is a family-owned and operated business based in Chicago, IL. We specialize in building custom garages to fit your unique needs and style preferences. 
+      <br/>
+      <br/>
+      Our team is committed to delivering high-quality workmanship and excellent customer service. From designing your dream garage to obtaining the necessary permits and completing the construction process, we handle every step of the project with professionalism and attention to detail. 
+      <br/>
+      <br/>
+      Let us help you transform your garage into a functional and stylish space that adds value to your home.</p>
     </div>
   )
 }
